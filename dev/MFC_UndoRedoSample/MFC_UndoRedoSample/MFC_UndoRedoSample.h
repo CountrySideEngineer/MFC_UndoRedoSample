d51f39c385cd0562d51e614527ef1a821d8dd5c0
CountrySideEngineer <deep-sleeper@nifty.com>
2019-09-21 17:24:27 +0900

    1st commit. -Add VisualStudio solution file and their development environment.  It is made by VisualStudioCommunity2019.

---
+++ dev/MFC_UndoRedoSample/MFC_UndoRedoSample/MFC_UndoRedoSample.h
@@ -0,0 +1,32 @@
+﻿
+// MFC_UndoRedoSample.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CMFCUndoRedoSampleApp:
+// このクラスの実装については、MFC_UndoRedoSample.cpp を参照してください
+//
+
+class CMFCUndoRedoSampleApp : public CWinApp
+{
+public:
+	CMFCUndoRedoSampleApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCUndoRedoSampleApp theApp;
